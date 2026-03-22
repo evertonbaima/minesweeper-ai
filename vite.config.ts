aa17a98f24fcf2cfff85e045453ab96ef5512552
Everton Baima da Silva Sousa <everton-baima@hotmail.com>
2026-03-22 14:52:54 -0300

    close #1 — project scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 5173,
+    host: '127.0.0.1',
+    port: 3000,
     strictPort: false,
     hmr: {
-      protocol: 'ws',
-      host: 'localhost',
+      host: '127.0.0.1',
     },
   },
   optimizeDeps: {
